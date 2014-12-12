19a8c8a869e4c8c2b70e8246fa341bec406c912b
Zane Shannon <zcs@smileslaughs.com>
2014-12-12 02:33:04 -0800

    okay the thing works.

--- ZCSHandedness.m
+++ ZCSHandedness.m
@@ -7,6 +7,27 @@
 //
 
 #import "ZCSHandedness.h"
+#import <CoreMotion/CoreMotion.h>
+
+#ifndef ZCSHandednessUpdateInterval
+#define ZCSHandednessUpdateInterval 1.0
+#endif
+
+#ifndef ZCSHandednessAmbiguityQuotient
+#define ZCSHandednessAmbiguityQuotient 0.05
+#endif
+
+@interface ZCSHandedness()
+
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (nonatomic) CMAcceleration lastAcceleration;
+@property (nonatomic) CMRotationRate lastRotation;
+
+- (void) outputAccelertionData:(CMAcceleration) acceleration;
+- (void) outputRotationData:(CMRotationRate) rotation;
+- (void) calculateHandedness;
+
+@end
 
 @implementation ZCSHandedness
 
@@ -31,10 +52,48 @@ static ZCSHandedness *shared = nil;
 - (ZCSHandedness *) init {
 	self = [super init];
 	if (! self) return nil;
-	
-	// TODO: start tracking device position and updating handedness
-	
+	self.motionManager = [[CMMotionManager alloc] init];
+	self.motionManager.accelerometerUpdateInterval = ZCSHandednessUpdateInterval;
+	self.motionManager.gyroUpdateInterval = ZCSHandednessUpdateInterval;
+	[self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue]
+											 withHandler:^(CMAccelerometerData  *accelerometerData, NSError *error) {
+												 [self outputAccelertionData:accelerometerData.acceleration];
+											 }];
+	[self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue]
+									withHandler:^(CMGyroData *gyroData, NSError *error) {
+										[self outputRotationData:gyroData.rotationRate];
+									}];
 	return self;
+}
+
+- (void) outputAccelertionData:(CMAcceleration) acceleration {
+	self.lastAcceleration = acceleration;
+	//NSLog(@"acceleration: [%f, %f, %f]", acceleration.x, acceleration.y, acceleration.z);
+	[self calculateHandedness];
+}
+- (void) outputRotationData:(CMRotationRate) rotation {
+	self.lastRotation = rotation;
+	// NSLog(@"rotation: [%f, %f, %f]", rotation.x, rotation.y, rotation.z);
+	[self calculateHandedness];
+}
+
+- (void) calculateHandedness {
+	
+	// TODO: improve this algorithm lol
+	
+	if (ABS(self.lastAcceleration.x) < ZCSHandednessAmbiguityQuotient) {
+		self.handedness = ZCSHandednessAmbiguousHandedness;
+	}
+	else if (self.lastAcceleration.x > 0) {
+		self.handedness = ZCSHandednessRightHandedness;
+	}
+	else if (self.lastAcceleration.x < 0) {
+		self.handedness = ZCSHandednessLeftHandedness;
+	}
+	else {
+		// very very rvre case
+		self.handedness = ZCSHandednessAmbiguousHandedness;
+	}
 }
 
 + (ZCSHandedness *)sharedHandedness {
