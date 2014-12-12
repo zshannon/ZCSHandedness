e4d9ab0ddc3ddc4197f8afb79bb148e3dc138716
Zane Shannon <zcs@smileslaughs.com>
2014-12-11 23:59:39 -0800

    init

---
+++ ZCSHandedness.h
@@ -0,0 +1,13 @@
+//
+//  ZCSHandedness.h
+//  ZCSHandednessExample
+//
+//  Created by Zane Shannon on 12/11/14.
+//  Copyright (c) 2014 Zane Shannon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZCSHandedness : NSObject
+
+@end
---
+++ ZCSHandedness.m
@@ -0,0 +1,13 @@
+//
+//  ZCSHandedness.m
+//  ZCSHandednessExample
+//
+//  Created by Zane Shannon on 12/11/14.
+//  Copyright (c) 2014 Zane Shannon. All rights reserved.
+//
+
+#import "ZCSHandedness.h"
+
+@implementation ZCSHandedness
+
+@end
